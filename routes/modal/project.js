a690332b9832883737c6f36916db67facef7b73d
ch04937 <97hernandez.c@gmail.com>
2020-04-18 20:07:35 -0500

    feat: added project endpoint

--- routes/modal/project.js
+++ routes/modal/project.js
@@ -2,8 +2,21 @@ const db = require("../../data/db-config");
 
 module.exports = {
 	getAll,
+	addProject,
+	findById,
 };
 
 function getAll() {
 	return db("projects");
+}
+function findById(id) {
+	return db("projects").where({ id }).first();
+}
+function addProject(project) {
+	return db("projects")
+		.insert(project, "id")
+		.then((ids) => {
+			const [id] = ids;
+			return findById(id);
+		});
 }
--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -7,6 +7,17 @@ route.get("/", async (req, res) => {
 		res.status(200).json({ projects });
 	} catch (e) {
 		console.log(e);
+		res.status(404).json({ message: "Whoopsie", e: e });
+	}
+});
+route.post("/", async (req, res) => {
+	const body = req.body;
+	try {
+		const projects = await Projects.addProject(body);
+		console.log("projects", projects);
+		res.status(200).json({ projects });
+	} catch (e) {
+		console.log(e);
 	}
 });
 
